f7214ffa7cc413725fa79ec6a59b16c3017ea688
Aayush0821 <vatsayush67@gmail.com>
2025-11-20 11:14:33 +0530

    added bearer token in frontend

--- server/middlewares/verifyJWT.js
+++ server/middlewares/verifyJWT.js
@@ -5,9 +5,7 @@ import User from "../models/user.models.js";
 export const verifyJWT =  async (req, res, next) => {
   const outerObject = {}
   try {
-    const token = req.body.token || req.cookies?.token || req.headers.authorization?.split(" ")[1];
-    console.log(req.cookies.token)
-    console.log(req.headers.authorization)
+    const token =  req.headers.authorization?.split(" ")[1] || req.cookies?.token ;
 
     if (!token) {
       throw new apiError(401, "Unauthorized - No Token Provided");
