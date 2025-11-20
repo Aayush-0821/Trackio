897d440b5eaef579ac65c95f435e723875c97ddc
Aayush0821 <vatsayush67@gmail.com>
2025-11-20 10:12:46 +0530

    added deployment fix

--- server/index.js
+++ server/index.js
@@ -32,7 +32,11 @@ connectDB()
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({ 
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+ }));
 
 app.get("/", (req, res) => {
   res.send("API working");
