6d85166e16e2668b674b6918f79209aafabdd792
Aayush0821 <vatsayush67@gmail.com>
2025-11-20 11:03:32 +0530

    cookie issue persists

--- server/middlewares/verifyJWT.js
+++ server/middlewares/verifyJWT.js
@@ -6,23 +6,27 @@ export const verifyJWT =  async (req, res, next) => {
   const outerObject = {}
   try {
     const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+    console.log(req.cookies.token)
+    console.log(req.headers.authorization)
 
     if (!token) {
       throw new apiError(401, "Unauthorized - No Token Provided");
     }
 
-    outerObject.token = token;
+    
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    outerObject.decoded = decoded;
+    
     const user=await User.findById(decoded.id).select("-password");
-    outerObject.user = user;
+    
     if(!user){
       return res.status(404).json({success:false,message:"User Not Found"});
     }
-    req.user = user;
     next();
   } catch (error) {
     console.error("JWT Verification Failed:", error.message);
+
+    outerObject.cookie = req.cookies;
+    outerObject.authorization = req.headers.authorization
     outerObject.error="error in jwt process somewhere"
     next(new apiError(401, JSON.stringify(outerObject)));
   }
